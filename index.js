f03fd6cf68cb0871b6bd64d55b1718dfbc7386d3
smillims <mrdenkil777@gmail.com>
2020-11-28 21:23:48 +0200

    Update file

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+/* 
+	Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
    Code repository: _put repo URL here_
    Web app: _put project's github pages URL here_
    */
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+  const inhabits = [
+	{
+		species: 'cat',
+		name: 'Murka',
+		surname: 'My hosts didn\'t give me a surname',
+		gender: 'female',
+		legs: 'Have 4 legs',
+		hands: 'Cats haven\'t hands :(',
+		saying: 'Hey, I\'m cat',
+	},
+	{
+		species: 'human',
+		name: 'Jorj',
+		surname: 'Kluni',
+		gender: 'male',
+		legs: 'Have 2 legs',
+		hands: 'Have 2 hands',
+		saying:'Did you see Ocean\'s Eleven?',
+	},
+	{
+		species: 'superhuman',
+		name: 'Peter',
+		surname: 'Parker',
+		gender: 'male',
+		legs: 'Have 2 legs',
+		hands: 'Have 2 hands, sometimes more :)',
+		saying: 'Your friendly neighborhood Spider-Man!',
+	},
+	{
+		species: 'guinea pig',
+		name: 'Darwin',
+		surname: 'My hosts didn\'t give me a surname',
+		gender: 'male',
+		legs: 'Have 4 legs',
+		hands: 'I haven\'t hands, srr',
+		saying: 'Set aside. I forbid touching the ferret.',
+	},
+]
 
+const returnString = (arr) => {
+	return arr.map((person) => {
+		 return Object.values(person).map(info => info).join("; ");
+	}).join("\n");
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+print(returnString(inhabits));
